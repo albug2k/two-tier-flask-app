d1b8d50a350bd87a6891c4965831aeec285a4a0c
albug2k <63882366+albug2k@users.noreply.github.com>
2024-09-28 18:52:36 -0400

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mrcloudbooktwotier" # Replace with your actual S3 bucket name
+    bucket = "twotieflaskapp" # Replace with your actual S3 bucket name
     key    = "EKS-Ajay/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
