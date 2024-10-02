54eb2faf1fca47fb76208e5471a9e3958a60b602
albug2k <63882366+albug2k@users.noreply.github.com>
2024-10-02 10:03:31 -0400

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "twotieflaskapp" # Replace with your actual S3 bucket name
-    key    = "EKS-Ajay/terraform.tfstate"
+    key    = "EKS-Alba/terraform.tfstate"
     region = "us-east-1"
   }
 }
